8430a972f5b430a5ef2d8e2f6a6d3c745b3e805e
JuliaSmolianska <i.julia.i1990@gmail.com>
2024-03-20 19:26:41 +0200

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ export const App = () => {
         <Route
           path="/register"
           element={
-            <PrivateRoute
+            <RestrictedRoute
               redirectTo="/contacts"
               component={<RegisterPage />}
             />
